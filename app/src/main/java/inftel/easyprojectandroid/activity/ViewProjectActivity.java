008db833990992077fe8a1b8313f8558cc4612a4
anotauntanto <ana.93.hg@gmail.com>
2016-04-07 16:38:34 +0200

    view project

--- app/src/main/java/inftel/easyprojectandroid/activity/ViewProjectActivity.java
+++ app/src/main/java/inftel/easyprojectandroid/activity/ViewProjectActivity.java
@@ -1,23 +1,65 @@
 package inftel.easyprojectandroid.activity;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
 
 import inftel.easyprojectandroid.R;
+import inftel.easyprojectandroid.fragment.TaskListFragment;
 
 public class ViewProjectActivity extends AppCompatActivity {
 
+    private FragmentTabHost mTabHost;
+    public static final int TODO = 1;
+    public static final int DOING = 2;
+    public static final int DONE = 3;
+
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_project);
+
+        //Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //TabHost
+        mTabHost = (FragmentTabHost) findViewById(R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.tabcontent);
+
+        Bundle todo = new Bundle();
+        todo.putInt("Tab", TODO);
+        mTabHost.addTab(mTabHost.newTabSpec(getString(R.string.to_do)).setIndicator(getString(R.string.to_do)),
+                TaskListFragment.class, todo);
+
+        Bundle doing = new Bundle();
+        doing.putInt("Tab", DOING);
+        mTabHost.addTab(mTabHost.newTabSpec(getString(R.string.doing)).setIndicator(getString(R.string.doing)),
+                TaskListFragment.class, doing);
+
+        Bundle done = new Bundle();
+        done.putInt("Tab", DONE);
+        mTabHost.addTab(mTabHost.newTabSpec(getString(R.string.done)).setIndicator(getString(R.string.done)),
+                TaskListFragment.class, done);
+
+        //custom the view
+        mTabHost.setCurrentTab(1);
+        mTabHost.setDrawingCacheBackgroundColor(getColor(R.color.primary));
+        /*
+        mTabHost.getTabWidget().getChildAt(0).setBackgroundColor(getColor(R.color.blue));
+        mTabHost.getTabWidget().getChildAt(1).setBackgroundColor(getColor(R.color.yellow));
+        mTabHost.getTabWidget().getChildAt(2).setBackgroundColor(getColor(R.color.green));*/
+
+        //FloatingActionButton
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,3 +71,6 @@ public class ViewProjectActivity extends AppCompatActivity {
     }
 
 }
+
+
+
---
+++ app/src/main/java/inftel/easyprojectandroid/fragment/TaskListFragment.java
@@ -0,0 +1,89 @@
+package inftel.easyprojectandroid.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+import inftel.easyprojectandroid.R;
+import inftel.easyprojectandroid.activity.ViewProjectActivity;
+import inftel.easyprojectandroid.adapter.RecyclerViewTaskAdapter;
+import inftel.easyprojectandroid.model.Tarea;
+
+
+public class TaskListFragment extends Fragment {
+
+    private View view;
+    private ArrayList<Tarea> taskList;
+    private RecyclerView recyclerView;
+    private RecyclerViewTaskAdapter adapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_task_list, container, false);
+
+        Bundle arguments = getArguments();
+
+        int tab = arguments.getInt("Tab");
+        if (tab == ViewProjectActivity.DOING) {
+           loadDoingTasks();
+        } else if (tab == ViewProjectActivity.DONE) {
+            loadDoneTasks();
+        } else if (tab == ViewProjectActivity.TODO) {
+            loadTodoTasks();
+        }
+        return view;
+    }
+
+
+    public void loadDoingTasks () {
+
+        taskList = new ArrayList<>();
+        Tarea t1 = new Tarea();
+        t1.setDescripcion("Esta es la descripción de la tarea 1");
+        t1.setEstado("doing");
+        t1.setNombre("Tarea número 1");
+        t1.setTiempo(new BigInteger("50"));
+        taskList.add(t1);
+
+        Tarea t2 = new Tarea();
+        t2.setDescripcion("Esta es la descripción de la tarea 2");
+        t2.setEstado("doing");
+        t2.setNombre("Tarea número 2");
+        t2.setTiempo(new BigInteger("70"));
+        taskList.add(t2);
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.taskRecyclerView);
+        adapter = new RecyclerViewTaskAdapter(taskList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    public void loadTodoTasks() {
+
+    }
+
+    public void loadDoneTasks(){
+
+    }
+
+}
