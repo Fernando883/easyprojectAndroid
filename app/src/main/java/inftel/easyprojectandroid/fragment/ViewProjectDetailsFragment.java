61a7b4105a9fece17e34ff6ebf0f5374057d1dd2
Carlos Salas <devel@reserland.com>
2016-04-13 16:39:04 +0200

    Tituto de app en mainActivity. Corrección de errores tras merge

--- app/src/main/java/inftel/easyprojectandroid/fragment/ViewProjectDetailsFragment.java
+++ app/src/main/java/inftel/easyprojectandroid/fragment/ViewProjectDetailsFragment.java
@@ -1,8 +1,7 @@
 package inftel.easyprojectandroid.fragment;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Pair;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -10,27 +9,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.List;
-
 import inftel.easyprojectandroid.R;
-import inftel.easyprojectandroid.interfaces.ServiceListener;
-import inftel.easyprojectandroid.model.EasyProjectApp;
 import inftel.easyprojectandroid.model.Proyecto;
 import inftel.easyprojectandroid.model.Usuario;
-import inftel.easyprojectandroid.service.ProjectService;
 
 /**
  * Created by anotauntanto on 9/4/16.
  */
-public class ViewProjectDetailsFragment extends Fragment implements ServiceListener {
+public class ViewProjectDetailsFragment extends Fragment {
 
-    private String idProject;
-    private String idUsuario;
-    private int proyectNumUsers;
-    private String proyectName;
 
     private View view;
-    private ProjectService projectService;
     private Proyecto project;
 
     @Override
@@ -46,33 +35,22 @@ public class ViewProjectDetailsFragment extends Fragment implements ServiceListe
                              Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_viewprojectdetails, container, false);
+        loadContent();
 
         return view;
     }
 
 
-    @Override
-    public void onObjectResponse(Pair<String, ?> response) {
-
-        if (response.first.equals("getProjectDetails")){
-            System.out.println("Ei ana");
-            project = (Proyecto) response.second;
-            loadContent();
-        }
-
-
+    public void setProject(Proyecto project) {
+        this.project = project;
     }
 
-    @Override
-    public void onListResponse(Pair<String, List<?>> response) {
-
-    }
 
     public void loadContent () {
 
         //projectName
         TextView projectName = (TextView) view.findViewById(R.id.projectNameInfo);
-        projectName.setText(proyectName);
+        projectName.setText(project.getNombreP());
 
         //projectDescripcion
         TextView projectDescription = (TextView) view.findViewById(R.id.projectDescriptionInfo);
